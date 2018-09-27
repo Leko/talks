83be89b41d56394a77a5c98a1036d8470fbfd16f
Leko <leko.noor@gmail.com>
2018-09-28 02:43:16 +0900

    :memo: Add expo-app-for-code-reading.mdx

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,16 @@
 import { future as theme } from "mdx-deck/themes";
 
+console.log(theme);
+
 export default {
   ...theme,
-  font: 'Roboto, sans-serif, "Noto Sans Japanese", "YuGothic", "Yu Gothic"'
+  font: 'Roboto, sans-serif, "Noto Sans Japanese", "YuGothic", "Yu Gothic"',
+  quote: {
+    "border-left": "6px solid #888",
+    paddingLeft: "20px",
+    fontSize: "0.8em"
+  },
+  pre: {
+    fontSize: "24px"
+  }
 };
