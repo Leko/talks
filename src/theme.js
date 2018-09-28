1bb7fdeec6c78d1498c11144fa32ef4150610524
Leko <leko.noor@gmail.com>
2018-09-28 11:47:47 +0900

    :lipstick: Fix fonts

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,32 @@
 import { future as theme } from "mdx-deck/themes";
 
+const fontFamily =
+  'Roboto, sans-serif, "Noto Sans Japanese", "YuGothic", "Yu Gothic"';
+const header = {
+  "text-transform": "none"
+};
+
 export default {
   ...theme,
-  font: 'Roboto, sans-serif, "Noto Sans Japanese", "YuGothic", "Yu Gothic"',
+  h1: {
+    ...(theme.h1 || {}),
+    ...header
+  },
+  h2: {
+    ...(theme.h2 || {}),
+    ...header
+  },
+  font: fontFamily,
   quote: {
-    "border-left": "6px solid #888",
+    borderLeft: "6px solid #888",
     paddingLeft: "20px",
-    fontSize: "0.8em"
+    fontSize: "0.8em",
+    fontWeight: "normal"
   },
   pre: {
     fontSize: "24px"
+  },
+  code: {
+    fontFamily
   }
 };
